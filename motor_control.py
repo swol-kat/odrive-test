cb1994555bce4dc81bfaa31801b64fca060a53aa
Arjun Gandhi <arjungandhi06@gmail.com>
2020-10-21 19:07:18 -0400

    changes

--- motor_control.py
+++ motor_control.py
@@ -4,10 +4,6 @@ import odrive
 
 
 odrv0.erase_configuration()
-try:    
-    odrv0.reboot()
-except:
-    print('reboot')
 
 
 
@@ -18,17 +14,17 @@ kv = 160
 
 cpr = 2048
 
-voltage = 12 
+voltage = odrv0.vbus_voltage 
 
 
 
 
-odrv0.axis0.motor.config.current_lim = 20
+odrv0.axis0.motor.config.current_lim = 10 
 odrv0.axis0.controller.config.vel_limit = .75 * cpr * voltage * kv 
 odrv0.config.brake_resistance = .05
 odrv0.axis0.motor.config.pole_pairs = pole_pairs
+odrv0.axis0.motor.config.torque_constant = 8.27 / kv
 odrv0.axis0.motor.config.motor_type = MOTOR_TYPE_HIGH_CURRENT
-odrv0.axis0.motor.config.calibration_current = 10.0 
 
 
 
@@ -39,10 +35,6 @@ odrv0.axis0.encoder.config.calib_range = 0.05
 
 
 odrv0.save_configuration() 
-try:    
-    odrv0.reboot()
-except:
-    print('reboot')
 
 #3
 
@@ -55,4 +47,4 @@ odrv0.axis0.controller.config.vel_gain = .0035
 odrv0.axis0.controller.config.pos_gain = 30
 odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-odrv0.axis0.controller.pos_setpoint = odrv0.axis0.controller.pos_setpoint + 1*cpr
+odrv0.axis0.controller.pos_setpoint = odrv0.axis0.controller.pos_setpoint + 1*cpro
